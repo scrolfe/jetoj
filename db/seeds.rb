01af545791ca3208a387ed3dc092ff8440fda117
Thomas Lee <thomasjlee@live.com>
2017-09-11 13:46:05 -0500

    seed data for posts and media, update posts and media views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'faker'
 
+# Events
 3.times do
   city = Faker::Address.city
   state = Faker::Address.state_abbr
@@ -11,5 +12,21 @@ require 'faker'
     description: "#{Faker::Lorem.paragraph}",
     location: "#{Faker::Address.street_address}, #{city}, #{state} #{zip}",
     date_time: "#{date_time}"
-              )
+    )
+end
+
+# Posts
+3.times do
+  Post.create(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+  )
+end
+
+# Media
+3.times do
+  Medium.create(
+    youtube_id: "Zp_NxCyagaw",
+    description: "#{Faker::Lorem.sentence}"
+  )
 end
