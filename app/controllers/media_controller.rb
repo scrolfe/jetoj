e95aafc1d5f5f35fde88cf944cc10cce2864446f
Thomas Lee <thomasjlee@live.com>
2017-09-12 12:06:06 -0500

    add new, create, edit, update, destroy actions to media, simple forms in views

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -12,6 +12,37 @@ class MediaController < ApplicationController
   end
 
   def create
-    @medium = Medium.new
+    @medium = Medium.new(medium_params)
+    if @medium.save
+      redirect_to @medium
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @medium = Medium.find(params[:id])
+  end
+
+  def update
+    @medium = Medium.find(params[:id])
+    if @medium.update(medium_params)
+      redirect_to @medium
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @medium = Medium.find(params[:id])
+    if @medium.destroy
+      redirect_to media_path
+    end
+  end
+
+  private
+
+  def medium_params
+    params.require(:medium).permit(:youtube_id, :description)
   end
 end
