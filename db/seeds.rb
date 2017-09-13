d9a18520a8e9066200960b974c44d86ace61ed72
Thomas Lee <thomasjlee@live.com>
2017-09-13 14:50:35 -0500

    add seed_assets and update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'faker'
 
+# Access seed_assets via: "#{Rails.root}/app/assets/seed_assets"
+
 # Events
 3.times do
   city = Faker::Address.city
@@ -11,7 +13,8 @@ require 'faker'
     name: "#{Faker::BossaNova.artist} in #{city}",
     description: "#{Faker::Lorem.paragraph}",
     location: "#{Faker::Address.street_address}, #{city}, #{state} #{zip}",
-    date_time: "#{date_time}"
+    date_time: "#{date_time}",
+    image: File.new("#{Rails.root}/app/assets/seed_assets/events_images/event_image.jpg")
     )
 end
 
@@ -20,6 +23,7 @@ end
   Post.create(
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph,
+    image: File.new("#{Rails.root}/app/assets/seed_assets/posts_images/post_image.jpg")
   )
 end
 
@@ -27,6 +31,19 @@ end
 3.times do
   Medium.create(
     youtube_id: "Zp_NxCyagaw",
-    description: "#{Faker::Lorem.sentence}"
+    description: "#{Faker::Lorem.sentence}",
+    image: File.new("#{Rails.root}/app/assets/seed_assets/media_images/medium_image.jpg")
   )
 end
+
+# Users
+User.create(  email: 'test@user.com',
+              password: 'foobar',
+              password_confirmation: 'foobar',
+              username: 'testuser',
+              first_name: 'test',
+              last_name: 'user',
+              bio: Faker::Lorem.sentence,
+              admin: true,
+              avatar: File.new("#{Rails.root}/app/assets/seed_assets/user_avatars/user_avatar.jpg")
+            )
