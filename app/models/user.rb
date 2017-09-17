003141688445b20bcbb99c64bd2a2fc11c68b348
Thomas Lee <thomasjlee@live.com>
2017-09-17 10:32:29 -0500

    update paperclip dimensions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   has_many :media
   has_many :posts
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/system/users/images/:style/missing.png"
+  has_attached_file :avatar, styles: { thumb: "160x160#" }, default_url: "/system/users/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
