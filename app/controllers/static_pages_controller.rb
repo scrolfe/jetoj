de86ae35624313873e117a0849111db285a81cc5
Sam Rolfe <scrolfe@gmail.com>
2017-09-18 14:50:34 -0500

    added staticpages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def admin
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/about'
+
+  get 'static_pages/home'
+
+  get 'static_pages/admin'
+
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root to: "posts#index"
   resources :events
