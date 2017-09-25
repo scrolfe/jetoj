4ebda18c276117b6b0820b86622bb07b94a564eb
Sam Rolfe <scrolfe@gmail.com>
2017-09-25 13:24:08 -0500

    added initial jquery

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,13 +3,32 @@ console.log('ready');
 // var jetoj_logo = $('#jetoj-logo')
 // var menu = $('menu')
 
+
+const originalClasses = {
+    
+}
+
+logoResize = () => {
+    $('#jetoj-logo').removeClass('col-xs-12');
+    $('#jetoj-logo').addClass('col-xs-5 col-xs-offset-1');
+}
+spacing = () => {
+    $('#jetoj-logo').css('padding', 0);
+}
+addJetojRight = () => {
+        $('.row').eq(0).append("<img src='https://s3.us-east-2.amazonaws.com/jetoj/jetoj-right.jpg' class='col-xs-2 col-xs-offset-4'>");
+}
+
+
+
 $(window).on('scroll', function(e) {
     if ($(window).scrollTop() > 400) {
-        $('#jetoj-logo').removeClass('col-xs-12');
-        console.log('removed');
-        console.log($('#jetoj-logo'));
-        $('#jetoj-logo').addClass('col-xs-5');
-        $('#jetoj-logo').addClass('col-xs-offset-1');
-        console.log('added');
-    };
+        logoResize();
+        spacing();
+        if ($('.row').eq(0).children().length === 1) {
+            addJetojRight();
+        }
+    } else {
+
+    }
 })
