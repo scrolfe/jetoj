d9510a2dd5e9830999681d3384cbe520f0f4d8e1
Thomas Lee <thomasjlee@live.com>
2017-09-25 10:32:34 -0500

    resolve merge conflict in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
 
+  root to: "static_pages#home"
   get 'static_pages/login'
-
   get 'static_pages/about'
-
   get 'static_pages/home'
-
   get 'static_pages/admin'
 
-<<<<<<< HEAD
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
-  root to: "static_pages#home"
-=======
   devise_for :users,
     skip: :registrations,
     controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
@@ -21,16 +15,11 @@ Rails.application.routes.draw do
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
   end
 
-  root to: 'posts#index'
->>>>>>> development
   resources :events
   resources :posts
   resources :media
   resources :performers
 
-<<<<<<< HEAD
-=======
   get 'admins/new_user' => 'admins#new_user', as: :admins_new_user
   post 'admins/create_user' => 'admins#create_user', as: :admins_create_user
->>>>>>> development
 end
