d5f0860dee46c7e26df65b89374a79d5ad352c1d
Sam Rolfe <scrolfe@gmail.com>
2017-09-25 12:04:14 -0500

    default header version made

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,15 @@
 console.log('ready');
 
+// var jetoj_logo = $('#jetoj-logo')
+// var menu = $('menu')
 
 $(window).on('scroll', function(e) {
     if ($(window).scrollTop() > 400) {
-        console.log('hello');
+        $('#jetoj-logo').removeClass('col-xs-12');
+        console.log('removed');
+        console.log($('#jetoj-logo'));
+        $('#jetoj-logo').addClass('col-xs-5');
+        $('#jetoj-logo').addClass('col-xs-offset-1');
+        console.log('added');
     };
 })
