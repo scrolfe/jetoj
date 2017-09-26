b02ac69884d03dcb38f1a8405664e7f9cf06339e
Sam Rolfe <scrolfe@gmail.com>
2017-09-26 18:07:49 -0500

    restructured header or bootstrap, mobile

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,34 +3,48 @@ console.log('ready');
 // var jetoj_logo = $('#jetoj-logo')
 // var menu = $('menu')
 
+//
+// $(window).on('scroll', function(e) {
+//     if ($(window).scrollTop > 400) {
+//         $('.row').eq(0).toggle('col-xs-5 col-xs-offset-1');
+//     }
+// })
 
-originalClasses = () => {
-    $('jetoj-logo').removeClass('col-xs-5 col-xs-offset-1')
-    $('jetoj-logo').addClass('col-xs-12');
-    $('.row').children().eq(1).remove()
+jetojRightProps = () => {
+    var icon = $('#jetoj-right');
+    icon.css('max-width', '100px');
+    icon.css('max-height', '100px');
 }
 
 logoResize = () => {
     $('#jetoj-logo').removeClass('col-xs-12');
     $('#jetoj-logo').addClass('col-xs-5 col-xs-offset-1');
-}
-spacing = () => {
     $('#jetoj-logo').css('padding', 0);
 }
 addJetojRight = () => {
-        $('.row').eq(0).append("<img src='https://s3.us-east-2.amazonaws.com/jetoj/jetoj-right.jpg' class='col-xs-2 col-xs-offset-4'>");
+        $('.row').eq(0).append("<img src='https://s3.us-east-2.amazonaws.com/jetoj/jetoj-right.jpg' class='col-xs-2 col-xs-offset-4' id='jetoj-right'>");
+}
+headerAffix = () => {
+    $('.nav').addClass('navbar-fixed-top');
 }
 
-
+originalClasses = () => {
+    $('#jetoj-logo').removeClass('col-xs-5 col-xs-offset-1');
+    $('#jetoj-logo').addClass('col-xs-12');
+    $('#jetoj-right').remove();
+    // $('.menu').css('position', 'static')
+}
 
 $(window).on('scroll', function(e) {
     if ($(window).scrollTop() > 400) {
         logoResize();
-        spacing();
+        headerAffix();
         if ($('.row').eq(0).children().length === 1) {
             addJetojRight();
+            jetojRightProps();
         }
     } else {
         originalClasses();
     }
+
 })
