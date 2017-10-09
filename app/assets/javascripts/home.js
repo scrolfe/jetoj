7e27078d3f27d2466e219f836902b81c5ac4366a
Thomas Lee <thomasjlee@live.com>
2017-10-08 21:46:40 -0500

    remove all placeholder_nav references, move logout link to admin dropdown, add performers link to both navs

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(document).on('turbolinks:load', () => {
   // carousel for chicago
   let slidesChicago = $(".chicagoPerformers");
   let slideCountChicago = slidesChicago.length;
