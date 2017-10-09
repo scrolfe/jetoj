a2517b2990facc37edf16515d815185175fe044b
Sam Rolfe <scrolfe@gmail.com>
2017-10-09 11:42:03 -0500

    worked on google maps

--- config/maps.rb
+++ config/maps.rb
@@ -2,10 +2,10 @@ require 'google_maps_service'
 
 # Setup global parameters
 GoogleMapsService.configure do |config|
-  config.key = 'AIzaSyAdm9VYfo9iyXB0KQUjfKx0HHWzzHA_1kA'
+  config.key = 'AIzaSyD5oCFsiuCkN1xIVZvzKKw4F10cBMTpfwU'
   config.retry_timeout = 20
   config.queries_per_second = 10
 end
 
 # Initialize client using global parameters
-gmaps = GoogleMapsService::Client.new
+# gmaps = GoogleMapsService::Client.new
